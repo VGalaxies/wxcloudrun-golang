9c3528bdea545ff104af0833d840ea02bf17a8b1
V_Galaxy <1904821183@qq.com>
2022-04-21 22:23:23 +0800

    create BookModel table

--- db/model/counter.go
+++ db/model/counter.go
@@ -1,6 +1,9 @@
 package model
 
-import "time"
+import (
+	"time"
+	"gorm.io/gorm"
+)
 
 // CounterModel 计数器模型
 type CounterModel struct {
@@ -8,4 +11,14 @@ type CounterModel struct {
 	Count     int32     `gorm:"column:count" json:"count"`
 	CreatedAt time.Time `gorm:"column:createdAt" json:"createdAt"`
 	UpdatedAt time.Time `gorm:"column:updatedAt" json:"updatedAt"`
+}
+
+// demo
+type BookModel struct {
+	gorm.Model
+	Name        string
+	Category    string
+	Author      string
+	ImageLink   string
+	Description string
 }
