64e77280e765e384815ec14eb37ffdcd1e9a4994
V_Galaxy <1904821183@qq.com>
2022-04-22 09:49:20 +0800

    fix init data

--- db/model/counter.go
+++ db/model/counter.go
@@ -28,22 +28,37 @@ type CategoryModel struct {
 }
 
 func InitBook() error {
-  res := db.Get().Create(&[]BookModel{
-    {Id: 1, Name: "Operating Systems: Three Easy Pieces", Category: 1, Author: "Remzi H. Arpaci-Dusseau", Description: ""},
-    {Id: 2, Name: "Models of Computation", Category: 2, Author: "Jeff Erickson", Description: ""},
-    {Id: 3, Name: "Fundamentals of Computer Graphics", Category: 3, Author: "Peter Shirley", Description: ""},
-	})
+	migrator := db.Get().Migrator()
+	if !migrator.HasTable(&BookModel{}) {
+    res := migrator.CreateTable(BookModel{})
+    if res != nil {
+      return res
+    }
+	}
 
-  return res.Error;
-}
-
-func InitCategory() error {
-  res := db.Get().Create(&[]CategoryModel{
-    {Id: 1, Name: "Operating System"},
-    {Id: 2, Name: "Theoretical Computer Science"},
-    {Id: 3, Name: "Computer Graphics"},
+	res := db.Get().Create(&[]BookModel{
+		{Id: 1, Name: "Operating Systems: Three Easy Pieces", Category: 1, Author: "Remzi H. Arpaci-Dusseau", Description: ""},
+		{Id: 2, Name: "Models of Computation", Category: 2, Author: "Jeff Erickson", Description: ""},
+		{Id: 3, Name: "Fundamentals of Computer Graphics", Category: 3, Author: "Peter Shirley", Description: ""},
 	})
 
 	return res.Error
 }
 
+func InitCategory() error {
+	migrator := db.Get().Migrator()
+	if !migrator.HasTable(&CategoryModel{}) {
+    res := migrator.CreateTable(CategoryModel{})
+    if res != nil {
+      return res
+    }
+	}
+
+	res := db.Get().Create(&[]CategoryModel{
+		{Id: 1, Name: "Operating System"},
+		{Id: 2, Name: "Theoretical Computer Science"},
+		{Id: 3, Name: "Computer Graphics"},
+	})
+
+	return res.Error
+}
